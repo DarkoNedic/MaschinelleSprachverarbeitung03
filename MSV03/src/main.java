f24a0284fc486b1cc1e4ab3fce43494093aa772a
Darko Nedić <darko1996@gmail.com>
2018-11-28 15:15:04 +0100

    added file parser and corpus

--- MSV03/src/main.java
+++ MSV03/src/main.java
@@ -1,9 +1,44 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 public class main {
+	
+	public static File[] read_folder(String path) {
+		File[] files = new File(path).listFiles();
+		return files;
+	}
+	
+	public static void load_file(File file) throws IOException {
+    	//String str = new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())), StandardCharsets.UTF_8);
+    	readAllLines(file, StandardCharsets.UTF_8);
+    	
+    }
+    
+    public static void readAllLines(File file, Charset cs) throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(file.toPath(), cs)) {
+            for (;;) {
+                String line = reader.readLine();
+                if (line == null)
+                    break;
+                System.out.println(line);
+            }
+        }
+    }
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		String path = "./brown_training";
+				
+		File[] files = read_folder(path);
 		
+		for (File file : files) {
+			load_file(file);
+		}
 	}
 
 }
