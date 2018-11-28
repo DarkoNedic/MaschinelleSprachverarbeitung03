32ee14a8a15a1fb53d12b045b76bea593623c4e3
Darko Nedić <darko1996@gmail.com>
2018-11-28 16:20:42 +0100

    added Line.get method

--- MSV03/src/Line.java
+++ MSV03/src/Line.java
@@ -2,8 +2,12 @@ import java.util.ArrayList;
 
 public class Line {
 
-	public ArrayList<String> line;
+	public static ArrayList<String> line;
 	public Line (ArrayList<String> line) {
 		this.line = line;
+	}
+	
+	public static String get(int index) {
+		return line.get(index);
 	}
 }
